a40eb0124afa9543edc14f30008314f2eb10558a
arodriguez133 <arodriguez133@icloud.com>
2021-01-24 07:56:00 -0600

    finished game function

--- index.js
+++ index.js
@@ -116,8 +116,22 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight, age){
+    if(weight <= 5 && age >= 1){
+      return weight * .05;
+    } else if(weight >= 6 && weight <= 10){
+      return weight * .04;
+    }else if(weight >= 11 && weight <= 15){
+      return weight * .03;
+    }else if(weight > 15){
+      return weight * .02;
+    }else if(age < 5/12){
+      return weight *.1;
+    }else if(age >= 4/12 && age <= 7/12){
+      return weigt * .05;
+    }else if(age >= 7/12 && age <= 1){
+      return weight * .04;
+    }
   }
 
 
@@ -144,7 +158,13 @@ Use the game function below to do the following:
 */
 
 function game(user, computer){
-    /*add your code here*/
+  if ((user == 'scissors' && computer == 'paper') || (user == 'rock' && computer == 'scissors') || (user == 'paper' && computer == 'rock')){
+    return 'you win!'
+  } else if ((computer == 'scissors' && user == 'paper') || (computer == 'rock' && user == 'scissors') || (computer == 'paper' && user == 'rock')){
+    return 'you lose!'
+  } else {
+    return 'it\'s a tie'
+  }
 }
   
   
